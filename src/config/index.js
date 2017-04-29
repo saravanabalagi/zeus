4c6c3d184a2a6d633a03525bac0130c4a820c251
Praveen Selvaraj <praveen.beatle@gmail.com>
2017-04-29 17:37:50 +0530

    max-len set to warn only

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,4 @@
 export default {
-  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY
-      || 'KrRqpgZihVHX7u4nFyKpeUbk6',
-  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET
-      || 'S7rgk9wLYpgbC9FMdEednrvA4igFGgftx9o0Z6tq1kqJTxaX5d',
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY || 'KrRqpgZihVHX7u4nFyKpeUbk6',
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET || 'S7rgk9wLYpgbC9FMdEednrvA4igFGgftx9o0Z6tq1kqJTxaX5d',
 };
