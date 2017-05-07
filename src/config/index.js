d9f2078a47e9c6e40125a9904f0d06b6d9bece0d
Praveen Selvaraj <praveen.beatle@gmail.com>
2017-05-07 09:30:23 +0530

    got new tokens for twitter

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 export default {
   baseApiUrl: process.env.BASE_API_URL || 'https://zeus-news.herokuapp.com',
   twitterBaseUri: 'https://api.twitter.com',
-  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY || 'KrRqpgZihVHX7u4nFyKpeUbk6',
-  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET || 'S7rgk9wLYpgbC9FMdEednrvA4igFGgftx9o0Z6tq1kqJTxaX5d',
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY || 'xfNpvmBbNXQS14gQooVZzMGUq',
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET || 'Yk4d15kVa7vP8SJ8O3w0JQ1TbUFizKBFtiso2CZZmxHQnsqeHL',
   bingSearchApiKey: process.env.BING_SEARCH_API_KEY || 'fe6823b580914336b27e8525e9049b66',
   newsApiKey: process.env.NEWS_API_KEY || '758b6587060b470bb10d77918b96b8fc',
   apiAiClientAccessToken: process.env.API_AI_CLIENT_ACCESS_TOKEN || '0ac255d56407456ba4f45e5f3680bcf4',
